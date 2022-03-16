0c9957f5512638ece340f469c017ab58c86bc7af
Jose Delgado <jdelgado1025@gmail.com>
2022-03-16 15:35:36 -0700

    Added page directive for the ID and retrieved a single record

--- Pages/Teams/Team.cshtml.cs
+++ Pages/Teams/Team.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using League.Data;
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -17,8 +18,18 @@ namespace League.Pages.Teams
             _context = context;
         }
 
-        public void OnGet()
+        public Team Team { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
         {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Team = await _context.Teams.FindAsync(id);
+
+            return Page();
         }
     }
 }
