1172c7c835ec807614a4ebfbe3e2547b0e2d922e
Jose Delgado <jdelgado1025@gmail.com>
2022-03-28 14:04:27 -0700

    added all players display with starters in bold + populated the Team dropdown list

--- Pages/Players/Index.cshtml.cs
+++ Pages/Players/Index.cshtml.cs
@@ -7,6 +7,7 @@ using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace League.Pages.Players
 {
@@ -21,12 +22,28 @@ namespace League.Pages.Players
 
         public List<Player> Players { get; set; }
 
+        [BindProperty (SupportsGet = true)]
+        public string SelectedTeam { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SelectedPosition { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SortField { get; set; }
+
         public SelectList AllTeams { get; set; }
         public SelectList AllPositions { get; set; }
         public SelectList SortBy { get; set; }
 
         public async Task OnGetAsync()
         {
+            Players = await _context.Players.ToListAsync();
+
+            var teamIds = from tms in _context.Teams
+                          orderby tms.TeamId
+                          select tms.TeamId;
+
+            AllTeams = new SelectList(await teamIds.ToListAsync());
         }
     }
 }
